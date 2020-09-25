90e4e882330a8842c4c46bfa48a1e60ee3d77b65
Nakanishi, Yusuke <nakanishiyusuke0000@gmail.com>
2020-09-25 11:28:45 +0900

    user 情報取得クエリ

--- browser/src/component/index.tsx
+++ browser/src/component/index.tsx
@@ -1,19 +1,33 @@
 import React from "react";
 
-import { useUseTestQuery } from "src/generated/graphql";
+import { useUserInfoQuery } from "src/generated/graphql";
 import gql from "graphql-tag";
 
 gql`
-  query useTest {
-    test {
+  query userInfo {
+    user_by_pk(id: 1) {
       id
+      profile
     }
   }
 `;
 
 export const Index = () => {
-  const { data } = useUseTestQuery();
+  const { data, loading } = useUserInfoQuery();
 
-  console.log(data);
-  return <div>this is index</div>;
+  if(loading) {
+    return  <div>Loading</div>
+  }
+
+  if(!data) {
+    return <div>No Data, Error</div>
+  }
+
+  if(!data.user_by_pk) {
+    return <div>no user matched</div>
+  }
+
+  const profileToShow = data.user_by_pk.profile
+
+  return <div>{profileToShow}</div>;
 };
