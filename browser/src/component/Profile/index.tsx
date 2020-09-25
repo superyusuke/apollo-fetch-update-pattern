6924b867ae0b96c1f7938a02156f8aefed7e73bc
Nakanishi, Yusuke <nakanishiyusuke0000@gmail.com>
2020-09-25 12:56:58 +0900

    index へコメント

--- browser/src/component/Profile/index.tsx
+++ browser/src/component/Profile/index.tsx
@@ -5,8 +5,17 @@ import { ProfileUI } from "src/component/Profile/UI";
 
 export const Profile = () => {
   return (
+    // provider = context を provide する
+    // つまり以下の値を provide する機構と値を provide する
+    // state と、それを変更する setState メソッド
+    // fetch してきたデータと、再フェッチする refetch メソッド
+    // そのほか provider 配下で必要なものがあれば任意の値を provide する
     <Provider>
+      {/* setter = fetch してきた data を setState する */}
+      {/* provider で set すればいいように一見見えるが、ループしてしまう */}
+      {/* provider で set してループしない実装があればプルリクいただきたい */}
       <Setter>
+        {/* 画面の構成要素を配置する */}
         <ProfileUI />
       </Setter>
     </Provider>
